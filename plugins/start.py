7bd42f46faf6a80178f2aac6ff7dcca074380afe
Frey <120489002+Haruto-hyuuga@users.noreply.github.com>
2023-04-10 15:47:12 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -137,13 +137,7 @@ async def not_joined(client: Client, message: Message):
         pass
 
     await message.reply(
-        text = FORCE_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
+        text = FORCE_MSG.format(message.from_user.mention),
         reply_markup = InlineKeyboardMarkup(buttons),
         quote = True,
         disable_web_page_preview = True
