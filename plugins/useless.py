c11f7e9653404ce1c9bd8ff1f723c54c648952e3
Frey <120489002+Haruto-hyuuga@users.noreply.github.com>
2023-04-10 17:49:07 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,4 +1,4 @@
-from bot import Bot
+from bot import Bot, APP
 from pyrogram.types import Message
 from pyrogram import filters
 from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
@@ -11,3 +11,7 @@ async def stats(bot: Bot, message: Message):
     delta = now - bot.uptime
     time = get_readable_time(delta.seconds)
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
+
+@APP.on_message(filters.command('st'))
+async def stats(_, message: Message):
+    await message.reply_text("hmmmm")
