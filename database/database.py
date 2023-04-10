12b9ee4815c78299aed78ac5af8c3e779a5163fb
Frey <120489002+Haruto-hyuuga@users.noreply.github.com>
2023-04-10 14:54:00 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,19 +1,10 @@
-#(©)CodeXBotz
-
-
-
-
 import pymongo, os
 from config import DB_URI, DB_NAME
 
 
 dbclient = pymongo.MongoClient(DB_URI)
-database = dbclient[DB_NAME]
-
-
-user_data = database['users']
-
-
+database = dbclient["USERs"]
+user_data = database['Uids']
 
 async def present_user(user_id : int):
     found = user_data.find_one({'_id': user_id})
@@ -21,6 +12,35 @@ async def present_user(user_id : int):
 
 async def add_user(user_id: int):
     user_data.insert_one({'_id': user_id})
+    return
+
+async def full_userbase():
+    user_docs = user_data.find()
+    user_ids = []
+    for doc in user_docs:
+        user_ids.append(doc['_id'])
+        
+    return user_ids
+
+async def del_user(user_id: int):
+    user_data.delete_one({'_id': user_id})
+    return
+
+
+prodatabase = dbclient["PREMIUM"]
+pro_user = prodatabase["Info"]
+
+async def present_user(user_id : int):
+    found = pro_user.find_one({'_id': user_id})
+    return bool(found)
+
+async def add_user(fullname: str, user_id: int, Username: str, DaTe: int):
+    pro_user.insert_one({
+        '_name': fullname,
+        '_id': user_id,
+        '_@': Username,
+        '_date': DaTe
+    })
     return
 
 async def full_userbase():
