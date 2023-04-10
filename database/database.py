c5ea1a65a641d8df300d5b7cd6d2241e9ea89d54
Frey <120489002+Haruto-hyuuga@users.noreply.github.com>
2023-04-10 14:54:52 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -30,11 +30,11 @@ async def del_user(user_id: int):
 prodatabase = dbclient["PREMIUM"]
 pro_user = prodatabase["Info"]
 
-async def present_user(user_id : int):
+async def present_PROuser(user_id : int):
     found = pro_user.find_one({'_id': user_id})
     return bool(found)
 
-async def add_user(fullname: str, user_id: int, Username: str, DaTe: int):
+async def add_PROuser(fullname: str, user_id: int, Username: str, DaTe: int):
     pro_user.insert_one({
         '_name': fullname,
         '_id': user_id,
@@ -43,14 +43,14 @@ async def add_user(fullname: str, user_id: int, Username: str, DaTe: int):
     })
     return
 
-async def full_userbase():
-    user_docs = user_data.find()
+async def full_PROuserbase():
+    user_docs = pro_user.find()
     user_ids = []
     for doc in user_docs:
         user_ids.append(doc['_id'])
         
     return user_ids
 
-async def del_user(user_id: int):
-    user_data.delete_one({'_id': user_id})
+async def del_PROuser(user_id: int):
+    pro_user.delete_one({'_id': user_id})
     return
