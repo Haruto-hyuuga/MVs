925302bb83b61116012c6228002aab46c5214dff
Frey <120489002+Haruto-hyuuga@users.noreply.github.com>
2023-04-27 19:54:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,16 +3,16 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 
-TG_BOT_TOKEN = ""
+TG_BOT_TOKEN = "6196393623:AAECNvlY25mNENWoC3dydzoRAMX7vuQIee4"
 APP_ID = 12585681
 API_HASH = "7741e8a55a0b5174548c52a374ab94b8"
 # main channel
-FORCE_SUB_CHANNEL = 
-CHANNEL_URL = "https://t.me/"
-DB_URI = ""
+FORCE_SUB_CHANNEL = -1001889883722
+CHANNEL_URL = "https://t.me/moviesociety2023"
+DB_URI = "mongodb+srv://gilaf95044:T77V0pRQSnhPyzF6@cluster0.otrixyr.mongodb.net/?retryWrites=true&w=majority"
 
 # database channel
-CHANNEL_ID = 
+CHANNEL_ID = -1001701712791
 CREATOR_GC = -1001977466101
 PORT = os.environ.get("PORT", "8080")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
@@ -23,12 +23,12 @@ anime_BOT_url = "https://t.me/AnimeDL_Robot"
 
 START_MSG = """Welcome {}
 Trough This Bot You Can Download Movies From Our Channel For Free!
-Search Movies on: 
+Search Movies on: @moviesociety2023
 """
 
 FORCE_MSG = """
 ONLY MEMBERS OF MY CHANNEL CAN USE THIS BOT.
-JOIN: @HORNYSOCIETY18
+JOIN: @moviesociety2023
 AND TRY AGAIN ♥️
 """
 
